835fae6f0819678f4eee2c3ebf597a74b7ebca5c
Marian Noekel <mariannoekel@gmail.com>
2026-03-14 12:22:58 +0100

    delete hovering desktop menu

--- app/components/navbar/desktop/Bridge.tsx
+++
@@ -1,5 +0,0 @@
-const Bridge = () => (
-  <div className="absolute -top-[24px] left-0 right-0 h-[24px]" />
-);
-
-export default Bridge
--- app/components/navbar/desktop/ShiftingDropDown.tsx
+++
@@ -1,9 +0,0 @@
-import Tabs from "./Tabs";
-
-export const ShiftingDropDown = () => {
-  return (
-    <div className="flex w-full justify-start ">
-      <Tabs />
-    </div>
-  );
-};
