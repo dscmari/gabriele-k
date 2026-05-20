8e9ebde1b67651313a29f78cc5f7feb019406688
Marian Noekel <mariannoekel@gmail.com>
2026-05-20 23:12:25 +0200

    implement onclick actions for desktop navbar contact infos

--- app/components/navbar/desktop/DesktopNavbar.tsx
+++ app/components/navbar/desktop/DesktopNavbar.tsx
@@ -13,14 +13,17 @@ export default function DesktopNavbar({ className }: Props) {
   return (
     <div className={`mx-auto p-8 pb-4 ${className}`}>
       <div className="flex gap-4 justify-end text-sm font-light pb-4 !text-custom-blue">
-        <div className="flex items-center gap-2 ">
+        <Link href="tel:+4915202076952" className="flex items-center gap-2 ">
           <Phone className="shrink-0 size-4" />
-          <span>+49 1520 2076952</span>
-        </div>
-        <div className="flex items-center gap-2">
+          <span className="!cursor-pointer"> +49 1520 2076952</span>
+        </Link>
+        <Link
+          href="mailto:info@gabrielakramer.de"
+          className="flex items-center gap-2"
+        >
           <Mail className="shrink-0 size-4" />
-          <span>info@gabrielakramer.de</span>
-        </div>
+          <span className="!cursor-pointer"> info@gabrielakramer.de</span>
+        </Link>
       </div>
       <div className="flex items-center gap-16 p-4 ">
         <Link className="z-100" href={"/"}>
@@ -33,12 +36,11 @@ export default function DesktopNavbar({ className }: Props) {
             className="w-36"
           />
         </Link>
-          <ShiftingDropDown />
-          <div className="flex items-center gap-8">
-            <ContactBtn />
-          </div>
+        <ShiftingDropDown />
+        <div className="flex items-center gap-8">
+          <ContactBtn />
+        </div>
       </div>
-         
     </div>
   );
 }
